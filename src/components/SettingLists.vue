f568149facffca0f896afd4d5a088c68c03e1863
Kazuki Miyahara <kmiya@outlook.com>
2017-03-17 12:00:26 +0900

    add folding buttons

--- src/components/SettingLists.vue
+++ src/components/SettingLists.vue
@@ -17,34 +17,43 @@
       </div>
     </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="res.open = !res.open">{{ res.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">Resources</h3>
-    <table class="table table-sm">
-      <thead>
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Directory or File</th>
-        <th scope="col">Security</th>
-        <th scope="col"></th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="(entry, index) in setting.resources.patterns">
-        <td>{{ index + 1}}</td>
-        <td v-for="key in setting.resources.column">
-          <input v-model="entry[key]" :value="entry[key]" type="text" class="form-control form-control-sm">
-        </td>
-        <td>
-          <button v-on:click="setting.resources.patterns.splice(index, 1)" type="button" class="delbtn btn btn-danger btn-sm float-right">del</button>
-        </td>
-      </tr>
-      </tbody>
-    </table>
-    <div class="clearfix">
-      <button v-on:click="$emit('add-new-resource');" type="button" class="addbtn btn btn-outline-primary btn-sm float-right">add</button>
+    <div v-show="res.open">
+      <table  class="table table-sm">
+        <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Directory or File</th>
+          <th scope="col">Security</th>
+          <th scope="col"></th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(entry, index) in setting.resources.patterns">
+          <td>{{ index + 1}}</td>
+          <td v-for="key in setting.resources.column">
+            <input v-model="entry[key]" :value="entry[key]" type="text" class="form-control form-control-sm">
+          </td>
+          <td>
+            <button @click="setting.resources.patterns.splice(index, 1)" type="button" class="delbtn btn btn-danger btn-sm float-right">del</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+      <div class="clearfix">
+        <button @click="$emit('add-new-resource');" type="button" class="addbtn btn btn-outline-primary btn-sm float-right">add</button>
+      </div>
     </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="login.open = !login.open">{{ login.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">Login</h3>
-    <div class="form-group row">
+    <div v-show="login.open">
+      <div class="form-group row">
       <label for="login-page" class="col-5 col-form-label">Login Page</label>
       <div class="col-7">
         <input v-model="setting.formLogin.loginPage" :value="setting.formLogin.loginPage" class="form-control form-control-sm" type="text" id="login-page">
@@ -54,9 +63,14 @@
         <input v-model="setting.formLogin.failureUrl" :value="setting.formLogin.failureUrl" class="form-control form-control-sm" type="text" id="login-failure">
       </div>
     </div>
+    </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="logout.open = !logout.open">{{ logout.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">Logout</h3>
-    <div class="form-group row">
+    <div v-show="logout.open">
+      <div class="form-group row">
       <label for="logout-success-url" class="col-5 col-form-label">Logout Success Url</label>
       <div class="col-7">
         <input v-model="setting.logout.successUrl" :value="setting.logout.successUrl" class="form-control form-control-sm" type="text" id="logout-success-url">
@@ -66,9 +80,14 @@
         <input v-model="setting.logout.deleteCookies" :value="setting.logout.deleteCookies" class="form-control form-control-sm" type="text" id="delete-cookies">
       </div>
     </div>
+    </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="policies.open = !policies.open">{{ policies.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">Access Policies</h3>
-    <table class="table table-sm">
+    <div v-show="policies.open">
+      <table class="table table-sm">
       <thead>
       <tr>
         <th scope="col">#</th>
@@ -84,13 +103,14 @@
           <input v-model="entry[key]" :value="entry[key]" type="text" class="form-control form-control-sm">
         </td>
         <td>
-          <button v-on:click="setting.interceptUrls.patterns.splice(index, 1)" type="button" class="delbtn btn btn-danger btn-sm float-right">del</button>
+          <button @click="setting.interceptUrls.patterns.splice(index, 1)" type="button" class="delbtn btn btn-danger btn-sm float-right">del</button>
         </td>
       </tr>
       </tbody>
     </table>
-    <div class="clearfix">
-      <button v-on:click="$emit('add-new-policy');" type="button" class="addbtn btn btn-outline-primary btn-sm float-right">add</button>
+      <div class="clearfix">
+        <button @click="$emit('add-new-policy');" type="button" class="addbtn btn btn-outline-primary btn-sm float-right">add</button>
+      </div>
     </div>
 
     <h3 class="h5">Exception Handler</h3>
@@ -101,8 +121,12 @@
       </div>
     </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="csrf.open = !csrf.open">{{ csrf.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">CSRF Protection</h3>
-    <div class="form-check">
+    <div v-show="csrf.open">
+      <div class="form-check">
       <div class="alert" v-bind:class="{'alert-danger': !setting.enableCSRFProtection}">
         <label class="form-check-label">
           <input class="form-check-input" type="checkbox" v-model="setting.enableCSRFProtection" />
@@ -112,16 +136,22 @@
         </label>
       </div>
     </div>
+    </div>
 
+    <span class="fold float-left">
+      <a class="fold text-muted" @click="misc.open = !misc.open">{{ misc.open ? '[-]' : '[+]' }}</a>
+    </span>
     <h3 class="h5">Misc</h3>
-    <div class="form-check">
+    <div v-show="misc.open">
+      <div class="form-check">
       <label class="form-check-label">
         <input class="form-check-input" type="checkbox" v-model="setting.putUserIdIntoMDC" />
         Put UserID into MDC
       </label>
     </div>
+    </div>
 
-  </div>
+  </div><!-- settings -->
 </template>
 
 <script>
@@ -130,12 +160,28 @@
     props: ['setting'],
     data() {
       return {
+        res: { open: true },
+        login: { open: true },
+        logout: { open: true },
+        policies: { open: true },
+        csrf: { open: true },
+        misc: { open: true },
       };
     },
   };
 </script>
 
 <style>
+.fold {
+  margin-left: -.95rem;
+  text-align: right;
+  vertical-align: middle;
+  font-family: Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+  font-size: .8rem;
+}
+.fold:hover {
+  text-decoration: underline;
+}
 .table > tbody > tr > td {
   vertical-align: middle;
 }
@@ -143,7 +189,7 @@
   line-height: .8rem;
 }
 .form-control {
-  font-size: .9rem;
+  font-size: .8rem;
   font-family: Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
 }
 .delbtn {
